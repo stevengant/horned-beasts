14c2d9709dbb203438ffefc00cf77dae4e6fa0f1
stevengant <sgant39@gmail.com>
2023-01-11 00:49:43 +0000

    Lab: Class 02

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Main from './Main';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Main from '../Main/Main';
 import './App.css';
 
 class App extends React.Component {
