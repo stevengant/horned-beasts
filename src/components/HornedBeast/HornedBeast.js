a82409ff59794a2ad54c5195b2df24f05732627f
stevengant <sgant39@gmail.com>
2023-01-13 00:55:41 +0000

    Lab: Class 04

--- src/components/HornedBeast/HornedBeast.js
+++ src/components/HornedBeast/HornedBeast.js
@@ -17,35 +17,35 @@ class HornedBeast extends React.Component {
     this.setState({
       favorites: this.state.favorites + 1,
     });
-  }
-
-  handleImgClick = () => {
     this.props.handleOpenModal(this.props.title, this.props.image_url, this.props.description)
   }
 
   render() {
-    return(
+    return (
       <>
-      <Col className="mt-4">
-        <Card className="h-100" onClick={this.handleImgClick}>
-          <Card.Title >
-            {this.props.title}
-          </Card.Title>
-          <Card.Text>
-            ❤️ {this.state.favorites} Favorites
-          </Card.Text>
-          <Card.Img variant="top"
-            style={{height:'260px'}}
-            onClick={this.handleHearts}  
-            title={this.props.title} 
-            src={this.props.image_url} 
-            alt={this.props.altText} 
-          />
-          <Card.Text>
-            {this.props.description}
-          </Card.Text>
-        </Card>
-      </Col>
+        <Col className="mt-4">
+          <Card className="h-100">
+            <Card.Title >
+              {this.props.title}
+            </Card.Title>
+            <Card.Text>
+              ❤️ {this.state.favorites} Favorites
+            </Card.Text>
+            <Card.Img variant="top"
+              style={{ height: '260px' }}
+              onClick={this.handleHearts}
+              title={this.props.title}
+              src={this.props.image_url}
+              alt={this.props.altText}
+              horns={this.props.horns}
+            />
+            <Card.Text>
+              {this.props.description}
+              <br/>
+              {this.props.horns}
+            </Card.Text>
+          </Card>
+        </Col>
       </>
     );
   }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,25 +2,91 @@ import React from "react";
 import HornedBeast from "../HornedBeast/HornedBeast";
 import Container from 'react-bootstrap/Container';
 import Row from "react-bootstrap/Row";
+import { Form } from "react-bootstrap";
 import './Main.css';
 
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sortedData: this.props.data,
+    }
+  }
+
+  handleSelect = (event) => {
+    let selected = event.target.value;
+
+    if(selected === '1')
+    {
+      let newData = this.props.data.filter(num => num.horns === 1);
+      this.setState({
+        sortedData: newData
+      });
+    }
+    if(selected === '2')
+    {
+      let newData = this.props.data.filter(num => num.horns === 2);
+      this.setState({
+        sortedData: newData
+      });
+    }
+    if(selected === '3')
+    {
+      let newData = this.props.data.filter(num => num.horns === 3);
+      this.setState({
+        sortedData: newData
+      });
+    }
+    if(selected === '>3')
+    {
+      let newData = this.props.data.filter(num => num.horns === 100);
+      this.setState({
+        sortedData: newData
+      });
+    } else if(selected === 'All')  {
+      this.setState({
+        sortedData: this.props.data
+      });
+    }
+  }
+
+
 
   render() {
     return (
       <>
         <main>
+          {/* <ListGroup>
+            {this.state.sortedData.map((num, idx) => {
+              return <ListGroup.Item key={idx}>{num}</ListGroup.Item>
+            })}
+          </ListGroup> */}
+          <Form>
+            <Form.Group>
+              <Form.Select name="selected" onChange={this.handleSelect}>
+                <option>Filter by Number of Horns</option>
+                <option value="All">All</option>
+                <option value="1">One Horn</option>
+                <option value="2">Two Horns</option>
+                <option value="3">Three Horns</option>
+                <option value=">3">More Than Three Horns</option>
+              </Form.Select>
+            </Form.Group>
+          </Form>
           <Container>
             <Row xs={1} sm={2} md={3} lg={4}>
-              {this.props.data.map((beast) => {
-                return <HornedBeast 
-                  title={beast.title} 
-                  image_url={beast.image_url} 
-                  description={beast.description} 
-                  key={beast._id}
-                  handleOpenModal={this.props.handleOpenModal} 
-                />
+              {this.state.sortedData.map((beast) => {
+                return (
+                  <HornedBeast
+                    title={beast.title}
+                    image_url={beast.image_url}
+                    description={beast.description}
+                    horns={beast.horns}
+                    key={beast._id}
+                    handleOpenModal={this.props.handleOpenModal}
+                  />
+                )
               })}
             </Row>
           </Container>
